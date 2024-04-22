0cc18f047c607c3d3a005598330a6bab8d07a575
rohan.gore <rohan.gore@cashfree.com>
2024-04-22 11:17:34 +0530

    Debounce added while calling API(handleIntersect)

--- src/pages/Home/components/MovieLists.jsx
+++ src/pages/Home/components/MovieLists.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef, useContext } from "react";
 import _get from "lodash/get";
 import _find from "lodash/find";
+import _debounce from "lodash/debounce";
 
 // Components
 import MovieCards from "./MovieCards";
@@ -10,10 +11,16 @@ import { getMovies } from "../../../services/movies";
 
 // Context
 import { Context } from "../../../context/Context";
+import Loader from "../../../components/Loader";
 
 const MovieLists = () => {
   const [movieData, setMovieData] = useState([]);
+
+  // TODO: Make year as [2012], so that when user scroll up and down will take year[] 1st or last element so that will have a track of year which has been fetch.
+  // now as it is a string when you scroll up and then try to scroll down it is trying to fetch the same year again as we dont have a track of it.
   const [year, setYear] = useState(2012);
+  const [loading, setLoading] = useState(false);
+  const [scrollDirection, setScrollDirection] = useState(null);
 
   const sentinelTopRef = useRef(null);
   const sentinelBottomRef = useRef(null);
@@ -23,19 +30,26 @@ const MovieLists = () => {
 
   useEffect(() => {
     // Scroll to a specific position when the page is reloaded
-    // window.scrollTo({ top: 50, behavior: "smooth" });
-  }, []); // This effect runs only once after the component mounts
+    // window.scrollTo({ top: 50 });
+  }); // This effect runs only once after the component mounts
 
   useEffect(() => {
     fetchData(year, selectedGenre);
   }, [year, selectedGenre]);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(handleIntersect, {
-      root: null,
-      rootMargin: "0px",
-      threshold: 1.0,
-    });
+    const debounceHandleIntersect = _debounce(handleIntersect, 1000);
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        debounceHandleIntersect(entries);
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 1.0,
+      }
+    );
 
     if (sentinelTopRef.current) {
       // invoke callback, on visible
@@ -65,10 +79,12 @@ const MovieLists = () => {
         if (entry.target.id === "sentinel-top") {
           // check for target id
           setYear((prevYear) => +prevYear + 1);
+          setScrollDirection("down");
         }
 
         if (entry.target.id === "sentinel-bottom") {
           setYear((prevYear) => +prevYear - 1);
+          setScrollDirection("up");
         }
       }
     });
@@ -77,9 +93,11 @@ const MovieLists = () => {
   const fetchData = async (year, selectedGenre) => {
     // TODO: check if we can handle already movie fetch should not call [not sure]
     // To checking if movie is already fetched
-    // if (_find(movieData, { year: year })) {
-    //   return;
-    // }
+    if (_find(movieData, { year: year })) {
+      return;
+    }
+
+    setLoading(true);
 
     const queryObj = { with_genres: selectedGenre };
 
@@ -106,7 +124,11 @@ const MovieLists = () => {
 
       return [{ year, data: _get(data, "results", []) }];
     });
+
+    setLoading(false);
   };
+
+  console.log({ movieData, year, scrollDirection });
 
   return (
     <>
@@ -115,6 +137,8 @@ const MovieLists = () => {
         id="sentinel-top"
         ref={sentinelTopRef}
       />
+
+      {loading && scrollDirection === "down" && <Loader />}
 
       <section className="p-5 mt-10" ref={scrollRef}>
         {movieData.map((movie) => (
